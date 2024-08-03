937aabad42cf96d387f382d2e6378151e5949d92
roundonut <133570949+roundonut@users.noreply.github.com>
2024-08-04 00:08:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1171,5 +1171,6 @@ def get_locations():
     return jsonify({'locations': locations})
     
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
